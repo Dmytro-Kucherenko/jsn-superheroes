994b11af5ebe0705dd28be66cce478bb5b9e71c3
Dmytro-Kucherenko <dmitro.kucherenko@gmail.com>
2023-10-21 14:25:37 +0300

    * client main app component import

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './libs/components';
+import { App } from './libs/components/components.js';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
