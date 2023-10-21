cbb09518e25043d2a7fa176bad4b90716a378d41
Dmytro-Kucherenko <dmitro.kucherenko@gmail.com>
2023-10-21 11:52:51 +0300

    * server main config and validation pipe

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
+import { AppModule } from './packages/app';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  app.useGlobalPipes(new ValidationPipe({
+    exceptionFactory: (errors) => {
+      const cause = errors.map((error) => error.constraints[Object.keys(error.constraints)[0]]);
+      return new BadRequestException('Validation is failed.', { cause });
+    },
+    transform: true,
+  }));
+  await app.listen(configService.get('PORT'));
 }
 
 bootstrap();
