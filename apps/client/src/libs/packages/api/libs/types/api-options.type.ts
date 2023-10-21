29661e07f2e27680f3d02f91c5d50975d8ad1ce4
Dmytro-Kucherenko <dmitro.kucherenko@gmail.com>
2023-10-21 14:27:50 +0300

    + client api options

---
+++ apps/client/src/libs/packages/api/libs/types/api-options.type.ts
@@ -0,0 +1,6 @@
+type ApiOptions = {
+  method: 'GET' | 'POST' | 'PUT' | 'DELETE';
+  payload: BodyInit | null;
+};
+
+export type { ApiOptions };
---
+++ apps/client/src/libs/packages/api/libs/types/types.ts
@@ -0,0 +1,1 @@
+export type { ApiOptions } from './api-options.type.js';
