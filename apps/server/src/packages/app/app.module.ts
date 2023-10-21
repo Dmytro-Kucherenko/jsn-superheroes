d621dedaad5d19b4d933f5475e87aef7de5b5577
Dmytro-Kucherenko <dmitro.kucherenko@gmail.com>
2023-10-21 12:02:33 +0300

    * server app module import heroes

--- apps/server/src/packages/app/app.module.ts
+++ apps/server/src/packages/app/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { databaseConfig } from '../../libs/packages/database';
+import { HeroesModule } from '../heroes';
 
 @Module({
   imports: [
+    HeroesModule,
     ConfigModule.forRoot(),
     TypeOrmModule.forRootAsync(databaseConfig),
   ],
