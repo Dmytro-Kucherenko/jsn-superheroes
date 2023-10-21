79e4fbffdb0373504abe02461161125ba5c756b1
Dmytro-Kucherenko <dmitro.kucherenko@gmail.com>
2023-10-21 14:24:45 +0300

    * vite env meta options

--- apps/client/src/vite-env.d.ts
+++ apps/client/src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_APP_DEVELOPMENT_PORT: string;
+  readonly VITE_APP_API_ORIGIN_URL: string;
+  readonly VITE_APP_PROXY_SERVER_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
