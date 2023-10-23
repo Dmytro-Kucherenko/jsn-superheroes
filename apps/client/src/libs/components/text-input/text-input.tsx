042e26a09012ceeecd5640c8f381e74ee16556ea
Dmytro-Kucherenko <dmitro.kucherenko@gmail.com>
2023-10-23 03:37:45 +0300

    + client textinput component

---
+++ apps/client/src/libs/components/text-input/text-input.tsx
@@ -0,0 +1,52 @@
+import {
+  FieldPath,
+  FieldValues,
+  UseControllerProps,
+  useController,
+} from 'react-hook-form';
+import './style.scss';
+
+const TextInput = <T extends FieldValues>({
+  controllerProps,
+  label,
+  placeholder,
+  disabled,
+  required,
+  rowsCount = 1,
+}: {
+  controllerProps: UseControllerProps<T, FieldPath<T>>;
+  label: string;
+  placeholder?: string;
+  disabled?: boolean;
+  required?: boolean;
+  rowsCount?: number;
+}) => {
+  const { field } = useController(controllerProps);
+
+  return (
+    <label className="input-container">
+      <span className="label">{label}</span>
+      {rowsCount === 1 ? (
+        <input
+          className="input"
+          type="text"
+          placeholder={placeholder}
+          disabled={disabled}
+          required={required}
+          {...field}
+        />
+      ) : (
+        <textarea
+          className="input"
+          placeholder={placeholder}
+          disabled={disabled}
+          required={required}
+          rows={rowsCount}
+          {...field}
+        />
+      )}
+    </label>
+  );
+};
+
+export { TextInput };
