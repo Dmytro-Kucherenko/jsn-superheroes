6d209877172cf6983cec59adf3ff653735138da1
Dmytro-Kucherenko <dmitro.kucherenko@gmail.com>
2023-10-23 03:32:28 +0300

    + client app dispatch, app selector

---
+++ apps/client/src/libs/hooks/index.ts
@@ -0,0 +1,2 @@
+export { useAppSelector } from './use-app-selector.hook.js';
+export { useAppDispatch } from './use-app-dispatch.hook.js';
---
+++ apps/client/src/libs/hooks/use-app-selector.hook.ts
@@ -0,0 +1,8 @@
+import { type TypedUseSelectorHook, useSelector } from 'react-redux';
+import { type store } from '../packages/store';
+
+const useAppSelector: TypedUseSelectorHook<
+  ReturnType<typeof store.instance.getState>
+> = useSelector;
+
+export { useAppSelector };
