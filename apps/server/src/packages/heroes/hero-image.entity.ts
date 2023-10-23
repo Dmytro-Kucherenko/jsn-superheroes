412fb7441ebe7070eb687aa0a3e747580c7e2cad
Dmytro-Kucherenko <dmitro.kucherenko@gmail.com>
2023-10-23 03:13:57 +0300

    + server hero image entity

---
+++ apps/server/src/packages/heroes/hero-image.entity.ts
@@ -0,0 +1,20 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+import { HeroEntity } from './hero.entity.js';
+import type { ContentType } from '../../libs/enums';
+
+@Entity('hero_images')
+class HeroImageEntity {
+  @PrimaryGeneratedColumn()
+  id!: number;
+  
+  @Column()
+  binary!: string;
+
+  @Column('varchar', {name: 'content_type'})
+  contentType!: ContentType;
+
+  @ManyToOne(() => HeroEntity, hero => hero.images, { onDelete: 'CASCADE' })
+  hero!: HeroEntity;
+}
+
+export { HeroImageEntity };
