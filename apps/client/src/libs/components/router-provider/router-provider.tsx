e3e00c0dc23c18901a9310767fc13b175ab53b1c
Dmytro-Kucherenko <dmitro.kucherenko@gmail.com>
2023-10-23 03:41:58 +0300

    + client router provider component

---
+++ apps/client/src/libs/components/router-provider/router-provider.tsx
@@ -0,0 +1,54 @@
+import {
+  RouterProvider as LibraryRouterProvider,
+  Navigate,
+  createBrowserRouter,
+} from 'react-router-dom';
+import { AppRoute } from '../../enums';
+import { App } from '../app/app.js';
+import { HeroesWrapper } from '../heroes-wrapper/heroes-wrapper.js';
+import { HeroesList } from '../../../pages/heroes-list/heroes-list.js';
+import { HeroCreate } from '../../../pages/hero-create/hero-create.js';
+import { HeroInfo } from '../../../pages/hero-info/hero-info.js';
+
+const RouterProvider: React.FC = () => {
+  const router = createBrowserRouter([
+    {
+      path: AppRoute.ROOT,
+      element: <App />,
+      children: [
+        {
+          path: AppRoute.ROOT,
+          element: (
+            <HeroesWrapper>
+              <HeroesList />
+            </HeroesWrapper>
+          ),
+        },
+        {
+          path: AppRoute.HERO_$ID,
+          element: (
+            <HeroesWrapper>
+              <HeroInfo />
+            </HeroesWrapper>
+          ),
+        },
+        {
+          path: AppRoute.HERO_CREATE,
+          element: (
+            <HeroesWrapper>
+              <HeroCreate />
+            </HeroesWrapper>
+          ),
+        },
+        {
+          path: AppRoute.OTHER,
+          element: <Navigate to={AppRoute.ROOT} />,
+        },
+      ],
+    },
+  ]);
+
+  return <LibraryRouterProvider router={router} />;
+};
+
+export { RouterProvider };
