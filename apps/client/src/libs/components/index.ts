ff86d9154f726bcde6c2da7477e3230347325c68
Dmytro-Kucherenko <dmitro.kucherenko@gmail.com>
2023-10-23 03:41:13 +0300

    *  client components exports

--- apps/client/src/libs/components/index.ts
+++ apps/client/src/libs/components/index.ts
@@ -1,1 +1,6 @@
 export { App } from './app/app.js';
+export { RouterProvider } from './router-provider/router-provider.js';
+export { Header } from './header/header.js';
+export { Loader } from './loader/loader.js';
+export { HeroForm } from './hero-form/hero-form.js';
+export { TextInput } from './text-input/text-input.js';
