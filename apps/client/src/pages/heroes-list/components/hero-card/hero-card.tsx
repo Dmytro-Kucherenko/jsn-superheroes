72444ac1c1cb2c9166a75984f6560b1bfe16401c
Dmytro-Kucherenko <dmitro.kucherenko@gmail.com>
2023-10-23 03:41:24 +0300

    + client hero card component

---
+++ apps/client/src/pages/heroes-list/components/hero-card/hero-card.tsx
@@ -0,0 +1,24 @@
+import { Link } from 'react-router-dom';
+import { AppRoute } from '../../../../libs/enums';
+import { HeroItemResponseDto } from '../../../../packages/heroes/types';
+import './style.scss';
+
+const HeroCard: React.FC<{ hero: HeroItemResponseDto }> = ({ hero }) => {
+  return (
+    <Link
+      className="card"
+      to={AppRoute.HERO_$ID.replace(':id', hero.id.toString())}
+    >
+      <img
+        className="image"
+        src={`data:${hero.images[0].contentType};base64,${hero.images[0].binary}`}
+        alt={`Image of the ${hero.nickname}`}
+      />
+      <div className="info">
+        <span className="title">{hero.nickname}</span>
+      </div>
+    </Link>
+  );
+};
+
+export { HeroCard };
