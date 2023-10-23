39ed5131b991e0fdeeaaa9b4e69493c5ed189be1
Dmytro-Kucherenko <dmitro.kucherenko@gmail.com>
2023-10-23 03:35:42 +0300

    + client button component

---
+++ apps/client/src/libs/components/button/button.tsx
@@ -0,0 +1,16 @@
+import './style.scss';
+
+const Button: React.FC<{
+  type?: 'submit' | 'button'
+  onClick?: () => void,
+  disabled?: boolean,
+  children: React.ReactNode,
+}> = ({ type = 'button', onClick, disabled, children }) => {
+  return (
+    <button className='button' type={type} onClick={onClick} disabled={disabled}>
+      {children}
+    </button>
+  );
+};
+
+export { Button };
