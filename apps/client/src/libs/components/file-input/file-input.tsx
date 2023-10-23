741322996e2de79609e17e2661b4dd5d32248c60
Dmytro-Kucherenko <dmitro.kucherenko@gmail.com>
2023-10-23 03:37:56 +0300

    + client fileinput component

---
+++ apps/client/src/libs/components/file-input/file-input.tsx
@@ -0,0 +1,52 @@
+import UploadIcon from '../../../assets/icons/upload.svg'
+import { FieldPath, FieldValues, PathValue, UseControllerProps, useController } from 'react-hook-form';
+import { ContentType } from '../../enums';
+import './style.scss';
+
+const FileInput = <T extends FieldValues>({
+  controllerProps,
+  onChange,
+  description,
+  extensions,
+  required,
+}: {
+  controllerProps: UseControllerProps<T, FieldPath<T>>;
+  onChange?: (value: PathValue<T, FieldPath<T>>, file: File) => unknown;
+  description: string;
+  extensions?: ContentType[];
+  required?: boolean;
+}) => {
+  const {
+    field: { onChange: onFileChnage, value },
+  } = useController(controllerProps);
+
+  const handleChange = async (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    const [file = null] = event.target.files ?? [];
+    event.currentTarget.value = '';
+
+    if (file) {
+      onChange ? onFileChnage(await onChange(value, file)) : onFileChnage(file);
+    }
+  };;
+
+  return (
+    <div className="drag-zone">
+      <div>
+        <img src={UploadIcon} height={80} width={80} alt="Upload icon" />
+      </div>
+      <span className="primary-text">Drag file or click here</span>
+      <span className="secondary-text">{description}</span>
+      <input
+        type="file"
+        className="file-input"
+        onChange={handleChange}
+        accept={extensions?.join(', ')}
+        required={required}
+      />
+    </div>
+  );
+};
+
+export { FileInput };
