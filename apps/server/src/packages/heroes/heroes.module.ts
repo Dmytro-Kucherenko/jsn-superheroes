6a0e7dc170e253717ca82b9046a754c0031e34a0
Dmytro-Kucherenko <dmitro.kucherenko@gmail.com>
2023-10-23 03:15:35 +0300

    * server heroes module entities

--- apps/server/src/packages/heroes/heroes.module.ts
+++ apps/server/src/packages/heroes/heroes.module.ts
@@ -4,9 +4,10 @@ import { HeroesController } from './heroes.controller.js';
 import { HeroesService } from './heroes.service.js';
 import { HeroEntity } from './hero.entity.js';
 import { HeroesRepository } from './heroes.repository.js';
+import { HeroImageEntity } from './hero-image.entity.js';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([HeroEntity])],
+  imports: [TypeOrmModule.forFeature([HeroEntity, HeroImageEntity])],
   controllers: [HeroesController],
   providers: [HeroesService, HeroesRepository],
 })
