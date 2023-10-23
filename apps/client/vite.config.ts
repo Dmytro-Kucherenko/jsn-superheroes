7586800b43ea7b532e4cf86dee3e28d57f00772d
Dmytro-Kucherenko <dmitro.kucherenko@gmail.com>
2023-10-23 03:02:04 +0300

    * client vite config

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -2,22 +2,12 @@ import { type ConfigEnv, defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 const config = ({ mode }: ConfigEnv): ReturnType<typeof defineConfig> => {
-  const {
-    VITE_APP_PROXY_SERVER_URL,
-    VITE_APP_API_ORIGIN_URL,
-    VITE_APP_DEVELOPMENT_PORT,
-  } = loadEnv(mode, process.cwd());
+  const { VITE_APP_DEVELOPMENT_PORT } = loadEnv(mode, process.cwd());
 
   return defineConfig({
     plugins: [react()],
     server: {
       port: Number(VITE_APP_DEVELOPMENT_PORT),
-      proxy: {
-        [VITE_APP_API_ORIGIN_URL]: {
-          target: VITE_APP_PROXY_SERVER_URL,
-          changeOrigin: true,
-        },
-      },
     },
   });
 };
