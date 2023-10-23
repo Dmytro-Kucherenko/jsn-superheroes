687cbc488291393122afe97788d5a21602a9d7c3
Dmytro-Kucherenko <dmitro.kucherenko@gmail.com>
2023-10-23 17:37:18 +0300

    * client main toast

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider } from './libs/components';
+import { RouterProvider, Toast } from './libs/components';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Toast />
     <RouterProvider />
   </React.StrictMode>,
 );
