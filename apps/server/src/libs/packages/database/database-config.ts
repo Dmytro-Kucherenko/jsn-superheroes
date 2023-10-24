7394920ca26b62fc3c82aedb1ea6e043fb97c879
Dmytro-Kucherenko <dmitro.kucherenko@gmail.com>
2023-10-24 14:56:15 +0300

    * server databse config

--- apps/server/src/libs/packages/database/database-config.ts
+++ apps/server/src/libs/packages/database/database-config.ts
@@ -1,22 +1,18 @@
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import {
-  TypeOrmModuleAsyncOptions,
-  TypeOrmModuleOptions,
-} from '@nestjs/typeorm';
+import type { TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
 
 const databaseConfig: TypeOrmModuleAsyncOptions = {
   imports: [ConfigModule],
-  useFactory: (config: ConfigService) =>
-    ({
-      username: config.getOrThrow('DB_USERNAME'),
-      password: config.getOrThrow('DB_PASSWORD'),
-      database: config.getOrThrow('DB_NAME'),
-      type: config.getOrThrow('DB_TYPE'),
-      host: config.getOrThrow('DB_HOST'),
-      port: config.getOrThrow('DB_PORT'),
-      autoLoadEntities: true,
-      synchronize: true,
-    }) as TypeOrmModuleOptions,
+  useFactory: (config: ConfigService) => ({
+    type: 'postgres',
+    username: config.getOrThrow('DB_USERNAME'),
+    password: config.getOrThrow('DB_PASSWORD'),
+    database: config.getOrThrow('DB_NAME'),
+    host: config.getOrThrow('DB_HOST'),
+    port: config.getOrThrow('DB_PORT'),
+    autoLoadEntities: true,
+    synchronize: true,
+  }),
   inject: [ConfigService],
 };
 
